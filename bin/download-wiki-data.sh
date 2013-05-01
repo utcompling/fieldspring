8085df036a1200c2eb1c3971b0bb3b783c9c167d
Mike Speriosu <mike@subtextsolutions.com>
2013-05-01 10:42:50 -0500

    Added download-wiki-data.sh.

---
+++ bin/download-wiki-data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget -r http://web.corral.tacc.utexas.edu/utcompling/fieldspring-data/wistr-models-cwardev-gt
+wget -r http://web.corral.tacc.utexas.edu/utcompling/fieldspring-data/wistr-models-cwartest-gt
+wget -r http://web.corral.tacc.utexas.edu/utcompling/fieldspring-data/wistr-models-trdev-gt
+wget -r http://web.corral.tacc.utexas.edu/utcompling/fieldspring-data/wistr-models-trtest-gt
+wget http://web.corral.tacc.utexas.edu/utcompling/fieldspring-data/wiki/enwiki-cwardev-20spd-100.log
+wget http://web.corral.tacc.utexas.edu/utcompling/fieldspring-data/wiki/enwiki-cwartest-20spd-100.log
+wget http://web.corral.tacc.utexas.edu/utcompling/fieldspring-data/wiki/trconlldev-100.log
+wget http://web.corral.tacc.utexas.edu/utcompling/fieldspring-data/wiki/trconlltest-100.log
